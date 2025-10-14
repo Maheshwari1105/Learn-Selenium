08be96fc309a497b90f5fa187a55d0e7577df8d6
Maheshwari1105 <mahi.arul1105@gmail.com>
2025-10-13 20:38:41 -0400

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+	String Name="John Doe";
+	int age=30;
+	float salary=55000.75f;
+	char gender= 'M';
+	boolean acceptTerms=true;
+	long mobileNumber= 9876543210L;
+	long pincode=56001l;
+	int luckyNumber=7;
+	System.out.println("Name=" +Name +"\n" + "Age=" +age +"\n" + "Salary=" +salary + "\n" + "Gender="+gender +"\n" + "Acceptterms=" +acceptTerms +"\n" +"MobileNumber="+mobileNumber+"\n"+"Pincode="+pincode +"\n"+"luckynumber="+luckyNumber);
+	
+	//System.out.println( "browserName=" +browserName + "\n" + "browserVersion =" + browserVersion + "\n" +"Logo="+browserLogo + "\n" +"Year=" +releaserYear +"\n" +"IsVisible=" +isVisible  );
+	
+				
+	
+	
+	
+	
+			
+	
+			
+
+	}
+
+}
